8a0e34f5ed9baf5cec087f80333331e17acd5758
acheb <achebeculpepper@gmail.com>
2025-09-26 02:57:39 -0400

    beta process

---
+++ pingeo/src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./message";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Message />
+    </>
+  );
+}
+
+export default App;
---
+++ pingeo/src/components/navbar.tsx
@@ -0,0 +1,7 @@
+import logo from "../assets/PinGeo_logo.png";
+
+function Navbar() {
+    return <img src={logo} alt="Logo" />;
+}
+
+export default Navbar;
